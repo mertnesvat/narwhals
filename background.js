6b71051ed4532b879542506c04acfa6209d37f3b
Mert Nesvat <mertnesvat@gmail.com>
2015-02-15 13:46:21 +0200

    loader eklendi, message send özelliği düzeltildi.

--- background.js
+++ background.js
@@ -1,61 +1,45 @@
 //alert('Warblers in action');
 var muge = "sezen+aksu";
+window.asd = "hello_world";
 var videoContentArray = Array();
-var mugeArray = Array();
 console.log('documentRead2');
 window.onload = function() { //sayfa yuklenince
-  var getTags = function(){ // getTags fonksiyonu tagleri alacak servis -> response -> action!
+  var getTags = function() { // getTags fonksiyonu tagleri alacak servis -> response -> action!
     var siteName = ""; //youtube / fizy kontrol
 
     var siteControl = document.domain;
     console.log(siteControl);
-    if(siteControl != "fizy.com"){
+    if (siteControl != "fizy.com") {
       siteName = "youtube";
-    }else{
+    } else {
       siteName = "fizy";
     }
 
     var videoContent = "";
-    if(siteName == "youtube"){
-      var metaTags=document.getElementsByTagName("meta");
+    if (siteName == "youtube") {
+      var metaTags = document.getElementsByTagName("meta");
       for (var i = 0; i < metaTags.length; i++) {
         if (metaTags[i].getAttribute("property") == "og:video:tag") { //video tag'inden alinan degerler tolgaya yolla!
 
           videoContent = metaTags[i].getAttribute("content");
-          console.log('videoContent = '+videoContent);
+          console.log('videoContent = ' + videoContent);
           videoContentArray.push(videoContent);
-          if(i == 0 && muge != "sezen+aksu" ){
-            muge = videoContent;
-          }
-          mugeArray.push(videoContent);
+
         }
       }
-    }else{
-    //  console.log('getting Child Node');
-    //  //var metaTags = document.getElementsByClassName('artist')[0].childNodes[0].firstChild;
-    //  var a = 0;
-    //  var metaTags = null;
-    //
-    //    metaTags = document.getElementsByClassName('artist');
-    //  for(var i = 0 ; i< metaTags.length; i++){
-    //    var innerTag = metaTags[i].childNodes[0].firstChild;
-    //    videoContentArray.push(innerTag);
-    //  }
+    } else {
+
     }
-    console.log(videoContentArray);
-    return videoContentArray;
+    var storage = chrome.storage.local;
+    storage.set({'tags': videoContentArray[0]}, function() {
+      // Notify that we saved.
+
+    });
   }
+  
 
 
-  //Register a button click handler that calls the background window.
-  //chrome.browserAction.setBadgeText({text : "selam"});
 
-
-  document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-      alert("stateChange");
-    }
-  }
 
 
 
@@ -72,4 +56,5 @@ window.onload = function() { //sayfa yuklenince
 
 
 
-};
+
+}
--- popup.js
+++ popup.js
@@ -3,110 +3,62 @@
 $(document).ready(function() {
 console.log('documentRead'+muge);
   //after button is clicked we download the data
-  var person = muge;
-  console.log("MMperson =" + person);
-
-  var searchQuery = 'followables/search?q='+person;
-  var baseEndpoint = "http://abitavsiyesiapp.com/curlDakick.php?parameters=";
-$.ajax({
-    url : baseEndpoint+searchQuery,
-    dataType : "text",
-    success: function(data) {
-     console.log(data);
-      var jsonParsePerson = $.parseJSON(data);
-      console.log('PersonID = '+jsonParsePerson.results[0].id);
-      var istanbulId = "207";
-      var personId = jsonParsePerson.results[0].id;
-      var searchUrl = "people/"+personId+"/events?location_ids="+istanbulId+"&page=1&per_page=10&locale=tr";
-          //people/1367234/events?location_ids=207&page=1&per_page=10&locale=tr'
-          //https://tr.dakick.com/api/v1/people/1367234/events?location_ids=207&page=1&per_page=10&locale=tr'
-      $.ajax({
-        url : baseEndpoint+searchUrl,
-        dataType : "text",
-        success: function(data) {
-            $('.loading').css('display','none');
-          //data downloaded so we call parseJSON function
-          //and pass downloaded data
-          var json = $.parseJSON(data);
-          //now json variable contains data in json format
-          //let's display a few items
-
-
-          //$('#results').html('Plugin name: ' + json.events[0].ticket_url + '<br />Author: ' + json.results);
-          console.log('1');
-          $.each(json.events, function (index, value) {
-            $('#inner').append("<li><img src=\"" + json.events[index].images.square.path + "\" class=\"img\">" +
-            "<div class=\"text\" > "+ json.events[index].name +"</div></li>").bind('click', function() {
-              console.log('2');
-              var newURL = json.events[index].ticket_url ;
-              console.log('3 = '+newURL);
-              chrome.tabs.create({ url: newURL });
-            });
-          })
+    var person = "mert";
+    var storage = chrome.storage.local;
+    storage.get('tags', function(items) {
+        console.log(items);
+        person = items.tags;
+        //person = "sezen aksu";
+        console.log("person ="+person);
+        var searchQuery = 'followables/search?q='+person;
+        var baseEndpoint = "http://abitavsiyesiapp.com/curlDakick.php?parameters=";
+        $.ajax({
+            url : baseEndpoint+searchQuery,
+            dataType : "text",
+            success: function(data) {
+                //console.log(data);
+                var jsonParsePerson = $.parseJSON(data);
+                console.log('PersonID = '+jsonParsePerson.results[0].id);
+                var istanbulId = "207";
+                var personId = jsonParsePerson.results[0].id;
+                var searchUrl = "people/"+personId+"/events?location_ids="+istanbulId+"&page=1&per_page=10&locale=tr";
+                //people/1367234/events?location_ids=207&page=1&per_page=10&locale=tr'
+                //https://tr.dakick.com/api/v1/people/1367234/events?location_ids=207&page=1&per_page=10&locale=tr'
+                $.ajax({
+                    url : baseEndpoint+searchUrl,
+                    dataType : "text",
+                    success: function(data) {
+                        $('.loading').css('display','none');
+                        //data downloaded so we call parseJSON function
+                        console.log(data);
+                        console.log("req = "+ baseEndpoint+searchUrl);
+                        //and pass downloaded data
+                        var json = $.parseJSON(data);
+                        //now json variable contains data in json format
+                        //let's display a few items
 
 
+                        //$('#results').html('Plugin name: ' + json.events[0].ticket_url + '<br />Author: ' + json.results);
 
-        }
-      });
-  }
-});
-    //start ajax request
-    //$.ajax({
-    //  url: "https://gist.githubusercontent.com/mertnesvat/45e63bd37c326f3d599e/raw/7d821cc57765721049efb658bba8c01171862b59/data.json",
-    //  //force to handle it as text
-    //  dataType: "text",
-    //  success: function(data) {
-    //
-    //    //data downloaded so we call parseJSON function
-    //    //and pass downloaded data
-    //    var json = $.parseJSON(data);
-    //    //now json variable contains data in json format
-    //    //let's display a few items
-    //
-    //
-    //    //$('#results').html('Plugin name: ' + json.events[0].ticket_url + '<br />Author: ' + json.results);
-    //    console.log('1');
-    //    $.each(json.events, function (index, value) {
-    //      $('#inner').append("<img src=\"" + json.events[index].images.square.path + "\">")
-    //      $('#inner').append( "<div id=\"red\" > "+ json.events[index].name +"</div>").bind('click', function() {
-    //        console.log('2');
-    //        var newURL = json.events[index].ticket_url ;
-    //        console.log('3 = '+newURL);
-    //        chrome.tabs.create({ url: newURL });
-    //      });
-    //    })
-    //
-    //
-    //
-    //  }
-    //});
+                        $.each(json.events, function (index, value) {
+                            $('#inner').append("<li><img src=\"" + json.events[index].images.square.path + "\" class=\"img\">" +
+                            "<div class=\"text\" > "+ json.events[index].name +"</div></li>").bind('click', function() {
+                                console.log('2');
+                                var newURL = json.events[index].ticket_url ;
+                                console.log('3 = '+newURL);
+                                chrome.tabs.create({ url: newURL });
+                            });
+                        })
+                    }
+                });
+            }
+        });
+    });
+
+
+
+
+
 
 });
 
-//function click(e) {
-//  var newURL = "http://stackoverflow.com/";
-//  var url = "https://gist.githubusercontent.com/mertnesvat/20ea185a6b7c697e7c28/raw/fc8b551dc992451f6864a9ad2d441dd306f10980/data.json";
-//  $.getJSON(url, function (json) {
-//    alert(json.result);
-//    $.each(json.list, function (i, fb) {
-//      alert(fb.result);
-//    });
-//  });
-//
-//  //var a = $('#red');
-//  console.log('asd');
-//  //alert(a);
-//
-//
-//  //chrome.tabs.create({ url: newURL });
-//
-//
-//}
-
-//document.addEventListener('DOMContentLoaded', function () {
-//  var divs = document.querySelectorAll('div');
-//  for (var i = 0; i < divs.length; i++) {
-//    divs[i].addEventListener('click', click);
-//  }
-//
-//});
