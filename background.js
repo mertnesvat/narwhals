7c7ca3ead267d506f605309833dce292c500c091
Mert Nesvat <mertnesvat@gmail.com>
2015-02-25 00:52:14 +0200

    final
    GTM!

--- background.js
+++ background.js
@@ -4,57 +4,35 @@ window.asd = "hello_world";
 var videoContentArray = Array();
 console.log('documentRead2');
 window.onload = function() { //sayfa yuklenince
-  var getTags = function() { // getTags fonksiyonu tagleri alacak servis -> response -> action!
-    var siteName = ""; //youtube / fizy kontrol
-
-    var siteControl = document.domain;
-    console.log(siteControl);
-    if (siteControl != "fizy.com") {
-      siteName = "youtube";
-    } else {
-      siteName = "fizy";
-    }
-
-    var videoContent = "";
-    if (siteName == "youtube") {
-      var metaTags = document.getElementsByTagName("meta");
-      for (var i = 0; i < metaTags.length; i++) {
-        if (metaTags[i].getAttribute("property") == "og:video:tag") { //video tag'inden alinan degerler tolgaya yolla!
-
-          videoContent = metaTags[i].getAttribute("content");
-          console.log('videoContent = ' + videoContent);
-          videoContentArray.push(videoContent);
-
+    var getTags = function() { // getTags fonksiyonu tagleri alacak servis -> response -> action!
+        var siteName = ""; //youtube / fizy kontrol
+        var siteControl = document.domain;
+        console.log(siteControl);
+        if (siteControl != "fizy.com") {
+            siteName = "youtube";
+        } else {
+            siteName = "fizy";
         }
-      }
-    } else {
-
+        var videoContent = "";
+        if (siteName == "youtube") {
+            var metaTags = document.getElementsByTagName("meta");
+            for (var i = 0; i < metaTags.length; i++) {
+                if (metaTags[i].getAttribute("property") == "og:video:tag") { //video tag'inden alinan degerler tolgaya yolla!
+                    videoContent = metaTags[i].getAttribute("content");
+                    console.log('videoContent = ' + videoContent);
+                    videoContentArray.push(videoContent);
+                }
+            }
+        } else {}
+        var storage = chrome.storage.local;
+        storage.set({
+            'tags': videoContentArray[0]
+        }, function() {
+            // Notify that we saved.
+        });
     }
-    var storage = chrome.storage.local;
-    storage.set({'tags': videoContentArray[0]}, function() {
-      // Notify that we saved.
-
-    });
-  }
-  
-
-
-
-
-
-
-  var content = getTags();
-
-  console.log('content = ' + content);
-
-  //servisden gelen veriler popup.htmlde doldurulacak.
-
-
-  //chrome.browserAction.setBadgeText({text : "2"}); // bu kod servisden gelen cevap kadar yazilacak!
-
-
-
-
-
-
+    var content = getTags();
+    console.log('content = ' + content);
+    //servisden gelen veriler popup.htmlde doldurulacak.
+    //chrome.browserAction.setBadgeText({text : "2"}); // bu kod servisden gelen cevap kadar yazilacak!
 }
--- popup.js
+++ popup.js
@@ -1,52 +1,49 @@
 //When DOM loaded we attach click event to button
-
 $(document).ready(function() {
-console.log('documentRead'+muge);
-  //after button is clicked we download the data
+    console.log('documentRead' + muge);
+    //after button is clicked we download the data
     var person = "mert";
     var storage = chrome.storage.local;
     storage.get('tags', function(items) {
         console.log(items);
         person = items.tags;
         //person = "sezen aksu";
-        console.log("person ="+person);
-        var searchQuery = 'followables/search?q='+person;
+        console.log("person =" + person);
+        var searchQuery = 'followables/search?q=' + person;
         var baseEndpoint = "http://abitavsiyesiapp.com/curlDakick.php?parameters=";
         $.ajax({
-            url : baseEndpoint+searchQuery,
-            dataType : "text",
+            url: baseEndpoint + searchQuery,
+            dataType: "text",
             success: function(data) {
                 //console.log(data);
                 var jsonParsePerson = $.parseJSON(data);
-                console.log('PersonID = '+jsonParsePerson.results[0].id);
+                console.log('PersonID = ' + jsonParsePerson.results[0].id);
                 var istanbulId = "207";
                 var personId = jsonParsePerson.results[0].id;
-                var searchUrl = "people/"+personId+"/events?location_ids="+istanbulId+"&page=1&per_page=10&locale=tr";
+                var searchUrl = "people/" + personId + "/events?location_ids=" + istanbulId + "&page=1&per_page=10&locale=tr";
                 //people/1367234/events?location_ids=207&page=1&per_page=10&locale=tr'
                 //https://tr.dakick.com/api/v1/people/1367234/events?location_ids=207&page=1&per_page=10&locale=tr'
                 $.ajax({
-                    url : baseEndpoint+searchUrl,
-                    dataType : "text",
+                    url: baseEndpoint + searchUrl,
+                    dataType: "text",
                     success: function(data) {
-                        $('.loading').css('display','none');
+                        $('.loading').css('display', 'none');
                         //data downloaded so we call parseJSON function
                         console.log(data);
-                        console.log("req = "+ baseEndpoint+searchUrl);
+                        console.log("req = " + baseEndpoint + searchUrl);
                         //and pass downloaded data
                         var json = $.parseJSON(data);
-                        //now json variable contains data in json format
+                        //now json variable contains data in json formatw
                         //let's display a few items
-
-
                         //$('#results').html('Plugin name: ' + json.events[0].ticket_url + '<br />Author: ' + json.results);
-
-                        $.each(json.events, function (index, value) {
-                            $('#inner').append("<li><img src=\"" + json.events[index].images.square.path + "\" class=\"img\">" +
-                            "<div class=\"text\" > "+ json.events[index].name +"</div></li>").bind('click', function() {
+                        $.each(json.events, function(index, value) {
+                            $('#inner').append("<li><img src=\"" + json.events[index].images.square.path + "\" class=\"img\">" + "<div class=\"text\" > " + json.events[index].name + "</div></li>").bind('click', function() {
                                 console.log('2');
-                                var newURL = json.events[index].ticket_url ;
-                                console.log('3 = '+newURL);
-                                chrome.tabs.create({ url: newURL });
+                                var newURL = json.events[index].ticket_url;
+                                console.log('3 = ' + newURL);
+                                chrome.tabs.create({
+                                    url: newURL
+                                });
                             });
                         })
                     }
@@ -54,11 +51,4 @@ console.log('documentRead'+muge);
             }
         });
     });
-
-
-
-
-
-
 });
-
